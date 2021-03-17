cd489f18dd354dd84d8ca70c6d845576d29ebb87
Neel Smith <dnsmith.neel@gmail.com>
2021-03-17 11:20:18 -0400

    Build histogram for all of Lysias

--- tokenize.jl
+++ tokenize.jl
@@ -2,35 +2,38 @@ using CitableText
 using Orthography
 using Unicode
 using FreqTables
+using PolytonicGreek
 
 f = "texts/lysias1.cex"
-c = CitableText.fromfile(CitableCorpus,f)
+output = "histo-1.cex"
 
 function cleangreek(s)
     normed = Unicode.normalize(s, :NFKC)
     Unicode.normalize(normed, stripmark=true)
 end
 
-lextokens = []
-for cn in c.corpus
-    tokenlist = PolytonicGreek.tokenizeLiteraryGreek(cn.text)
-    for t in tokenlist
-        if (t.tokencategory ==  Orthography.LexicalToken())
-            push!(lextokens, cleangreek(t.text))
+function lextokens(src)
+    c = CitableText.fromfile(CitableCorpus,f)
+    lextokens = []
+    for cn in c.corpus
+        tokenlist = PolytonicGreek.tokenizeLiteraryGreek(cn.text)
+        for t in tokenlist
+            if (t.tokencategory ==  Orthography.LexicalToken())
+                push!(lextokens, cleangreek(t.text))
+            end
         end
     end
+    lextokens
 end
 
-
-
-hist = sort(freqtable(lextokens); rev=true)
-
-
-output = []
-for n in names(hist)[1]
-    push!(output, string(n,"|", hist[n]))
-end
-
-open("hist.cex","w")  do io
-    println(io, join(output, "\n"))
+function histogram(src, target)
+    tkns = lextokens(src)
+    hist = sort(freqtable(tkns); rev=true)
+    output = []
+    for n in names(hist)[1]
+        push!(output, string(n,"|", hist[n]))
+    end
+    open(target,"w")  do io
+        println(io, join(output, "\n"))
+    end
 end
